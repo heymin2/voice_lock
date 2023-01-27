f3dbae3022fac27e0549cdefd655bb2bb9ff0538
heymin2 <131025_@naver.com>
2023-01-27 14:49:57 +0900

    로그인 디비에 저장 안된 아이디일 경우 예외처리

--- web/app.py
+++ web/app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 import pymysql
-from werkzeug.utils import secure_filename
 
 import librosa
 import IPython.display as ipd
@@ -9,8 +8,10 @@ import librosa.display
 
 import os
 
-app = Flask(__name__)
+from siamese import train
 
+app = Flask(__name__)
+app.secret_key = 'serseas'
 
 @app.route('/')
 def main():
@@ -26,7 +27,7 @@ def sign():
         passwd = request.form.get('passwd')
         voice1 = id+'.png'
         voice2 = id+'(1).png'
-        sql = "INSERT INTO voiceInfo (id, pass, voice1, voice2) VALUES (%s, %s, %s, %s)"
+        sql = "INSERT INTO voiceInfo (id, pass, voice1, voice2) VALUES (%s, %s, %s, %s);"
         cursor.execute(sql, (id, passwd, voice1, voice2))
         db.commit()
         return redirect(url_for('login'))
@@ -42,6 +43,18 @@ def signimg():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    train()
+    if request.method == 'POST':
+        db = pymysql.connect(host='localhost', port=3306, user='nemin',
+                         password='1234', db='voice', charset='utf8')
+        cursor = db.cursor()
+        id = request.form.get('id')
+        sql = "select count(id) from voiceInfo where id = %s;"
+        cursor.execute(sql, id)
+        rows = cursor.fetchall()
+        if rows[0][0] == 0:
+            flash("아이디가 존재하지 않습니다.")
+
     return render_template('login.html')
 
 
@@ -55,12 +68,12 @@ def loginimg():
 def sign_file(value):
     file = request.files[value]  # blob 파일 저장
 
-    os.makedirs('./' + file.filename, exist_ok=True)  # 폴더 생성
+    os.makedirs('./img/' + file.filename, exist_ok=True)  # 폴더 생성
 
-    file.save(os.path.join('./' + file.filename,
+    file.save(os.path.join('./img/' + file.filename,
                            file.filename + '.wav'))  # 폴더 위치에 파일 저장
 
-    audio_path = './' + file.filename + '/' + file.filename + '.wav'  # 오디오 파일 경로
+    audio_path = './img/' + file.filename + '/' + file.filename + '.wav'  # 오디오 파일 경로
 
     y, sr = librosa.load(audio_path)  # lbrosa.load() : 오디오 파일을 로드한다.
 
@@ -76,25 +89,25 @@ def sign_file(value):
     librosa.display.waveshow(y=y, sr=sr)
     plt.plot(y)
 
-    if os.path.exists('./' + file.filename + '/' + file.filename + ".png") == True:  # 파일 중복시
-        plt.savefig('./' + file.filename + '/' +
+    if os.path.exists('./img/' + file.filename + '/' + file.filename + ".png") == True:  # 파일 중복시
+        plt.savefig('./img/' + file.filename + '/' +
                     file.filename + '(1)' + ".png")
     else:
-        plt.savefig('./' + file.filename + '/' +
+        plt.savefig('./img/' + file.filename + '/' +
                     file.filename + ".png")  # png 파일 저장
 
-    os.remove('./' + file.filename + '/' +
+    os.remove('./img/' + file.filename + '/' +
               file.filename + '.wav')  # 음성 파일 제거
 
 
 def login_file(value):
     file = request.files[value]  # blob 파일 저장
 
-    if (os.path.isdir('./' + file.filename) == True):
-        file.save(os.path.join('./' + file.filename,
-                               file.filename + '.wav'))  # 폴더 위치에 파일 저장
+    if (os.path.isdir('./img/' + file.filename) == True):
+        file.save(os.path.join('./img/' + file.filename,
+                               file.filename + '_login.wav'))  # 폴더 위치에 파일 저장
 
-        audio_path = './' + file.filename + '/' + file.filename + '.wav'  # 오디오 파일 경로
+        audio_path = './img/' + file.filename + '/' + file.filename + '_login.wav'  # 오디오 파일 경로
 
         y, sr = librosa.load(audio_path)  # lbrosa.load() : 오디오 파일을 로드한다.
 
@@ -109,11 +122,12 @@ def login_file(value):
         plt.figure(figsize=(16, 6))
         librosa.display.waveshow(y=y, sr=sr)
         plt.plot(y)
-        plt.savefig('./' + file.filename + '/' +
-                    file.filename + "(1).png")  # png 파일 저장
+        plt.savefig('./img/' + file.filename + '/' +
+                    file.filename + "_login.png")  # png 파일 저장
 
-        os.remove('./' + file.filename + '/' +
-                  file.filename + '.wav')  # 음성 파일 제거
+        os.remove('./img/' + file.filename + '/' +
+                  file.filename + '_login.wav')  # 음성 파일 제거
+
 
 
 if __name__ == '__main__':
